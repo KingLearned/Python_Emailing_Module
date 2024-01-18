ae1d51628343b41595c066640100fb4a807e5a64
kinglearned <kinglearned@gmail.com>
2024-01-18 11:33:55 +0100

    Modified the main.py

--- main.py
+++ main.py
@@ -7,30 +7,30 @@ from SecretKey import EMAIL, PASSWORD
 from Message import main_body
 
 # Email configuration
-# sender_email = EMAIL
-# subject = "FULL-STACK WEB DEVELOPER INTERNSHIP"
-# body = main_body()
-# attachment_path = "Nelson-C_Resume.pdf"
+sender_email = EMAIL
+subject = "FULL-STACK WEB DEVELOPER INTERNSHIP"
+body = main_body()
+attachment_path = "Nelson-C_Resume.pdf"
 
-# bulk_mails = ['chithomzzy']
+bulk_mails = ['chithomzzy']
 
-# for mails in bulk_mails:
-#     receiver_email = mails
+for mails in bulk_mails:
+    receiver_email = mails
 
-#     message = MIMEMultipart()
-#     message["From"] = sender_email
-#     message["To"] = receiver_email
-#     message["Subject"] = subject
-#     message.attach(MIMEText(body, "plain"))
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
 
-#     with open(attachment_path, "rb") as attachment:
-#         part = MIMEBase("application", "octet-stream")
-#         part.set_payload(attachment.read())
-#         encoders.encode_base64(part)
-#         part.add_header("Content-Disposition", f"attachment; filename= {attachment_path}")
-#         message.attach(part)
+    with open(attachment_path, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header("Content-Disposition", f"attachment; filename= {attachment_path}")
+        message.attach(part)
 
-#     with smtplib.SMTP("smtp.gmail.com", 587) as server:
-#         server.starttls()
-#         server.login(sender_email, PASSWORD)
-#         server.sendmail(sender_email, receiver_email, message.as_string())
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(sender_email, PASSWORD)
+        server.sendmail(sender_email, receiver_email, message.as_string())
